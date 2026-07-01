706dffb133b7ecb5f09dbe34b9c8c3c5cd9d13cb
ESHADASH37 <eshadash407@gmail.com>
2026-07-01 13:08:26 +0530

    move zeros to end-easy

---
+++ Arrays/movezeros.java
@@ -0,0 +1,43 @@
+public class movezeros {
+    public static void movezero(int[]nums){
+        int n=nums.length;
+        int j=-1;
+
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                j=i;
+                break;
+            }
+        }
+        if(j==-1)
+            return;
+
+        for(int i=j+1;i<n;i++){
+            if(nums[i]!=0){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            j++;
+        
+        }
+    }
+    
+}
+public static void main(String args[]){
+    int[] arr = {1, 0, 2, 3, 0, 4, 0, 1};
+    System.out.print("before");
+    printArray(arr);
+
+    movezero(arr);
+
+    System.out.print("after moving zeros");
+    printArray(arr);
+}
+private static void printArray(int[] arr){
+    for (int num:arr){
+        System.out.print(num+"");
+    }
+    System.out.println();
+}
+}
+
