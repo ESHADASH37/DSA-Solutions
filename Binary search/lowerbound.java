df0349a8285487ea41e0405d0485f9b7cd80458b
ESHADASH37 <eshadash407@gmail.com>
2026-07-07 23:11:02 +0530

    Lower Bound-easy

---
+++ Binary search/lowerbound.java
@@ -0,0 +1,29 @@
+public class lowerbound {
+    public static int findlowerbound(int [] arr , int target){
+        int low=0;
+        int high = arr.length - 1;
+        int ans = arr.length;
+
+        while(low<=high){
+            int mid = low+(high -  low)/2;
+
+            if(arr[mid]>=target){
+                ans = mid;
+                high = mid -1;
+            }else{
+                low=mid + 1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String args[]){
+        int arr[]={1, 2, 4, 6, 6, 6, 8, 9};
+        int target=6;
+        int index=findlowerbound(arr,target);
+
+        System.out.println("The lower bound of "+target+"is at index:"+index);
+        int targetMissing = 5;
+        int indexMissing = findlowerbound(arr, targetMissing);
+        System.out.println("The lower bound of " + targetMissing + " is at index: " + indexMissing);
+    } 
+}
